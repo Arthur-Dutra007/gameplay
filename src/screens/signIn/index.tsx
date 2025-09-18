42c7ee23e385e83424d52597ccdd81bebede983b
Arthur-Dutra007 <arthurdutradesena@gmail.com>
2025-09-18 07:47:17 -0300

    aaaa
    aaaa

--- src/screens/signIn/index.tsx
+++ src/screens/signIn/index.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import 
 
 export  function SignIn() {
   return (
@@ -10,11 +11,3 @@ export  function SignIn() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
